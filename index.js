3a3cc49f21f941227864f7dfac3788e8d9ed84d7
cfedersp <charles.federspiel@gmail.com>
2022-07-22 17:29:36 -0700

    removed writing property files

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const io = require('@actions/io');
-const fs = require('fs');
 const codeArtifact = require('@aws-sdk/client-codeartifact');
 
 async function run() {
@@ -9,7 +7,6 @@ async function run() {
   const domain = core.getInput('domain', { required: true });
   const owner = core.getInput('owner', { required: true });
   const duration = core.getInput('duration', { required: false });
-  const type = core.getInput('type', { required: true });
   const repo = core.getInput('repo', { required: true });
 
   const client = new codeArtifact.CodeartifactClient({ region: region });
@@ -27,44 +24,7 @@ async function run() {
     throw Error(`AWS CodeArtifact Authentication Failed: ${response.$metadata.httpStatusCode} (${response.$metadata.requestId})`);
   }
 
-  switch(type) {
-    case 'gradle':
-      gradle(domain, owner, region, repo, authToken);
-      break;
-    case 'npm':
-      await npm(domain, owner, region, repo, authToken);
-      break;
-  }
-
   core.setOutput('registry', `https://${domain}-${owner}.d.codeartifact.${region}.amazonaws.com`);
+  core.setOutput('auth-token', authToken);
   core.setSecret(authToken);
-}
-
-async function npm(domain, owner, region, repo, authToken) {
-  const file = `
-registry=https://${domain}-${owner}.d.codeartifact.${region}.amazonaws.com/npm/${repo}/
-//${domain}-${owner}.d.codeartifact.${region}.amazonaws.com/npm/${repo}/:_authToken=${authToken}
-//${domain}-${owner}.d.codeartifact.${region}.amazonaws.com/npm/${repo}/:always-auth=true
-`;
-  io.rmRF('.npmrc');
-  fs.writeFile(`.npmrc`, file, { flag: 'wx' }, (callback) => {
-    if (callback) core.setFailed(callback);
-  });
-}
-
-async function gradle(domain, owner, region, repo, authToken) {
-  const file = `
-codeartifactToken=${authToken}
-`
-  io.rmRF('gradle.properties');
-  fs.writeFile(`gradle.properties`, file, { flag: 'wx' }, (c) => {
-    if (c) core.setFailed(c);
-  });
-}
-
-module.exports = run;
-
-/* istanbul ignore next */
-if (require.main === module) {
-    run();
 }
