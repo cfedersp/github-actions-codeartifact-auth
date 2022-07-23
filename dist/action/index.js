341f45f349a5334409ef8a374024c5f5b9fb5cdc
cfedersp <charles.federspiel@gmail.com>
2022-07-22 20:16:39 -0700

    setFailed

--- dist/action/index.js
+++ dist/action/index.js
@@ -1877,6 +1877,7 @@ async function run() {
   const owner = core.getInput('owner', { required: true });
   const duration = core.getInput('duration', { required: false });
   const repo = core.getInput('codeartifact-repo', { required: true });
+  core.setFailed('stopping here');
   throw Error('stop here');
 
   const client = new codeArtifact.CodeartifactClient({ region: region });
@@ -1891,7 +1892,10 @@ async function run() {
   const response = await client.send(authCommand);
   const authToken = response.authorizationToken;
   if (response.authorizationToken === undefined) {
-    throw Error(`AWS CodeArtifact Authentication Failed: ${response.$metadata.httpStatusCode} (${response.$metadata.requestId})`);
+    
+    const errMsg = `AWS CodeArtifact Authentication Failed: ${response.$metadata.httpStatusCode} (${response.$metadata.requestId})`;
+    core.setFailed(errMsg);
+    throw Error(errMsg);
   }
 
   core.setOutput('registry', `https://${domain}-${owner}.d.codeartifact.${region}.amazonaws.com`);
