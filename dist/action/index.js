4f2dfff734711a0293c81296c6f603dc0ffd13d8
cfedersp <charles.federspiel@gmail.com>
2022-07-22 17:54:08 -0700

    specify module type

--- dist/action/index.js
+++ dist/action/index.js
@@ -1,74 +1,12 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ 475:
-/***/ ((module) => {
-
-module.exports = eval("require")("@actions/core");
-
-
-/***/ }),
-
-/***/ 404:
-/***/ ((module) => {
-
-module.exports = eval("require")("@actions/exec");
-
-
-/***/ }),
-
-/***/ 279:
-/***/ ((module) => {
-
-module.exports = eval("require")("@aws-sdk/client-codeartifact");
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __nccwpck_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		var threw = true;
-/******/ 		try {
-/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
-/******/ 			threw = false;
-/******/ 		} finally {
-/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
-/******/ 		}
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-const core = __nccwpck_require__(475);
-const exec = __nccwpck_require__(404);
-const codeArtifact = __nccwpck_require__(279);
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const codeArtifact = require('@aws-sdk/client-codeartifact');
 
 async function run() {
   const region = core.getInput('region', { required: true });
@@ -97,8 +35,3 @@ async function run() {
   core.setSecret(authToken);
 }
 
-})();
-
-module.exports = __webpack_exports__;
-/******/ })()
-;
